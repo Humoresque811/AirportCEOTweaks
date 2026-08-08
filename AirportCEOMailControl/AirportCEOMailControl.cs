a32069ba3855e8e98aba6de15cdf408344df6419
Guus Dohmen <guusdohmen@hotmail.nl>
2026-08-08 20:16:46 +0200

    Added e-mail control

---
+++ AirportCEOMailControl/AirportCEOMailControl.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AirportCEOMailControl
+{
+    [BepInPlugin("org.airportceomailcontrol.Guusje2", "AirportCEOMailControl", "1.0.0")]
+    [BepInDependency("org.airportceomodloader.humoresque")]
+    [BepInDependency("org.airportceotweakscore.zeke")]
+    public class AirportCEOMailControl : BaseUnityPlugin
+    {
+        public static AirportCEOMailControl Instance { get; private set; }
+        internal static Harmony Harmony { get; private set; }
+        internal static ManualLogSource TweaksLogger { get; private set; }
+        internal static ConfigFile ConfigReference { get; private set; }
+
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            Harmony.PatchAll();
+
+            Instance = this;
+            TweaksLogger = Logger;
+            ConfigReference = Config;
+
+            // Config
+            Logger.LogInfo($"{PluginInfo.PLUGIN_GUID} is setting up config.");
+            AirportCEOMailControlConfig.SetUpConfig();
+            Logger.LogInfo($"{PluginInfo.PLUGIN_GUID} finished setting up config.");
+        }
+
+        // This is code for BepInEx logging, which Tweaks doesn't really use. Here if necessary
+        internal static void Log(string message) => LogInfo(message);
+        internal static void LogInfo(string message) => TweaksLogger.LogInfo(message);
+        internal static void LogError(string message) => TweaksLogger.LogError(message);
+        internal static void LogWarning(string message) => TweaksLogger.LogWarning(message);
+        internal static void LogDebug(string message) => TweaksLogger.LogDebug(message);
+    }
+}
+
---
+++ AirportCEOMailControl/AirportCEOMailControlConfig.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirportCEOMailControl
+{
+    internal class AirportCEOMailControlConfig
+    {
+        internal static ConfigEntry<bool> EnableOperationalReportMail { get; private set; }
+        internal static ConfigEntry<bool> EnableFinancialReportMail { get; private set; }
+        internal static ConfigEntry<bool> EnableEmergencyReportMail { get; private set; }
+        internal static ConfigEntry<bool> EnableContractNegotiationReminderMail { get; private set; }
+        internal static ConfigEntry<bool> DebugMode { get; private set; }
+        internal static void SetUpConfig()
+        {
+            EnableOperationalReportMail = ConfigRef.Bind("Mail", "Enable operational report e-mail", true, "Whether to enable operational report mail.");
+            EnableFinancialReportMail = ConfigRef.Bind("Mail", "Enable financial report e-mail", true, "Whether to enable financial report mail.");
+            DebugMode = ConfigRef.Bind("Debug", "Debug mode", false, "Whether to enable debug mode, adds verbose logging.");
+            //EnableEmergencyReportMail = ConfigRef.Bind("Mail", "EnableEmergencyReportMail", true, "Whether to enable emergency report mail.");
+            EnableContractNegotiationReminderMail = ConfigRef.Bind("Mail", "Enable contract negotiation reminder e-mail", true, "Whether to enable contract negotiation reminder mail.");
+        }
+
+        private static ConfigFile ConfigRef => AirportCEOMailControl.ConfigReference;
+    }
+}
